0946fbb0a5dfe31b75463fc37fdeea1772e0e411
JV-BugMaker <794241063@qq.com>
2017-12-10 18:36:11 +0800

    - 爬虫

---
+++ update/class_list.js
@@ -0,0 +1,37 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var debug = require('debug')('blog:update');
+
+
+debug('读取博文类别成功');
+
+//获取博客首页
+request('http://blog.sina.cn/u/1776757314',function(err,res){
+    if(err){
+        return console.error(err);
+    }
+    //根据网页内容进行DOM操作
+    var $ = cheerio.load(res.body.toString());
+
+    //读取博文类别列表
+    var classList = [];
+    $('.classList li a').each(function(){
+        var $me = $(this);
+        var item = {
+            name:$me.text().trim(),
+            url:$me.attr('href')
+        };
+        //从url中取出分类的ID
+        var s = item.url.match(/articlelist_\d+_(\d+)_\d\.html/);
+    
+        if(Array.isArray(s)){
+            item.id = s[1];
+            classList.push(item);
+        }
+    });
+
+    
+
+    //输出结果
+    console.log(classList);
+})
